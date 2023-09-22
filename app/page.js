467bb112d720b4ada8cbe09c19c8255a9919b394
surajsinghgore <surajthakurrs45@gmail.com>
2023-09-22 09:39:12 +0530

    -feat ui successfully created

--- app/page.js
+++ app/page.js
@@ -1,6 +1,5 @@
 
 import Image from 'next/legacy/image'
-import styles from './page.module.css'
 import Link from 'next/link'
 
 export default function Home() {
@@ -8,15 +7,17 @@ export default function Home() {
     
    <div className='container'>
 
-<div className="card">
+<div className="card"><Link href={""} target='_blank'>
 <div className="image_container">
   <Image src="https://static01.nyt.com/images/2023/09/21/multimedia/21murdoch-notebook-01-hfqk/21murdoch-notebook-01-hfqk-threeByTwoSmallAt2X.jpg" alt="a Image" layout='fill'/>
 </div>
 <div className="article">
-<Link href={""} target='_blank'>
-  <h1>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nam, voluptatum.</h1></Link>
+
+  <h1>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nam, voluptatum.</h1>
+</div></Link>
 </div>
-</div>
+
+
 
 
    </div>
